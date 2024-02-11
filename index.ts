bf74142538645b5606974163611d885bb7209903
Sachin Raja <me@s4n.land>
2024-02-11 12:02:42 -0800

    add download script

--- index.ts
+++
@@ -1,4 +0,0 @@
-import { readFileSync, writeFileSync } from 'node:fs'
-
-const text = JSON.parse(readFileSync('public/parking-lots.geojson', 'utf-8'))
-writeFileSync('public/parking-lots.json', JSON.stringify(text), 'utf-8')
